7c7107ca59bafb9c32ea6b23e26d4ab6ebd0a610
pedrosnaves <pedrosnaves@gmail.com>
2025-04-15 11:34:02 -0300

    Create PessoaFisica.java

---
+++ lab_herancas/PessoaFisica.java
@@ -0,0 +1,33 @@
+public class PessoaFisica extends Pessoas {
+    private String cpf;
+    private char sexo;
+    private int estadoCivil;
+
+    public PessoaFisica(String nome, String endereco, String telefone, String cpf, char sexo, int estadoCivil) {
+        super(nome, endereco, telefone);
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public char getSexo() {
+        return this.sexo;
+    }
+
+    public int getEstadoCivil() {
+        return this.estadoCivil;
+    }
+
+    public String toString() {
+        return "\nNome: " + getNome() +
+                "\nEndereço: " + getEndereco() +
+                "\nTelefone: " + getTelefone() +
+                "\nCPF: " + this.cpf +
+                "\nSexo: " + this.sexo +
+                "\nEstado Civil: " + this.estadoCivil;
+    }
+}
