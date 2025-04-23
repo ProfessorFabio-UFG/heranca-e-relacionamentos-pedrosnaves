01ff696e111ff7b8c707a7ba9f6ae570a70c951b
pedrosnaves <pedrosnaves@gmail.com>
2025-04-22 21:02:47 -0300

    Update Fornecedor.java

--- lab_herancas/Fornecedor.java
+++ lab_herancas/Fornecedor.java
@@ -1,14 +1,15 @@
 public class Fornecedor extends PessoaJuridica{
     private String produtos;
 
-    public Fornecedor(String nome, String endereco, String telefone, String cnpj, String razaoSocial, String produtos){
-        super(nome, endereco, telefone, cnpj, razaoSocial);
+    public Fornecedor(PessoaJuridica pj, String produtos){
+        super(pj, pj.getCnpj(), pj.getRazaoSocial());
         this.produtos = produtos;
     }
 
 
     public String toString() {
-        return super.toString() +
+        return "\nFornecedor: \n" +
+                super.toString() +
                 "\nProdutos: " + this.produtos;
 
     }
