668327ef74592e896074f75f3916cd0066b66472
pedrosnaves <pedrosnaves@gmail.com>
2025-04-22 21:07:04 -0300

    Update PessoaFisica.java

--- lab_herancas/PessoaFisica.java
+++ lab_herancas/PessoaFisica.java
@@ -1,25 +1,31 @@
-public class PessoaJuridica extends Pessoas{
-    private String cnpj;
-    private String razaoSocial;
+public class PessoaFisica extends Pessoas {
+    private String cpf;
+    private char sexo;
+    private int estadoCivil;
 
-    public PessoaJuridica(Pessoas p, String cnpj, String razaoSocial) {
+    public PessoaFisica(Pessoas p, String cpf, char sexo, int estadoCivil) {
         super(p.getNome(), p.getEndereco(), p.getTelefone());
-        this.cnpj = cnpj;
-        this.razaoSocial = razaoSocial;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
     }
 
-    public String getCnpj() {
-        return cnpj;
+    public String getCpf() {
+        return this.cpf;
     }
 
-    public String getRazaoSocial() {
-        return razaoSocial;
+    public char getSexo() {
+        return this.sexo;
+    }
+
+    public int getEstadoCivil() {
+        return this.estadoCivil;
     }
 
     public String toString() {
         return super.toString() +
-                "\nCNPJ: " + this.cnpj +
-                "\nRazão Social: " + this.razaoSocial;
+                "\nCPF: " + this.cpf +
+                "\nSexo: " + this.sexo +
+                "\nEstado Civil: " + this.estadoCivil;
     }
-
 }
